076bd16f116abf0419f5f6804269d605f9ad4c32
Pieterjan Spoelders <pj.spoelders@gmail.com>
2021-10-14 10:35:50 +0200

    changed comment style

--- error-reporting-csharp/ExaError.cs
+++ error-reporting-csharp/ExaError.cs
@@ -1,9 +1,9 @@
 namespace Exasol.ErrorReporting
 {
 
-    /**
-     * Facade for building Exasol error messages.
-     */
+    /// <summary>
+    /// Facade for building Exasol error messages.
+    /// </summary>
     public class ExaError
     {
 
@@ -12,12 +12,6 @@ namespace Exasol.ErrorReporting
             // empty on purpose
         }
 
-        /**
-         * Get a builder for error messages.
-         * 
-         * @param errorCode Exasol error code
-         * @return built error message
-         */
         public static ErrorMessageBuilder MessageBuilder(in string errorCode)
         {
             return new ErrorMessageBuilder(errorCode);
