f50104b7ff0744e144474093a914946eff610a2d
Pieterjan Spoelders <pj.spoelders@gmail.com>
2021-11-26 16:37:37 +0100

    simplified workflow

--- cd.bat
+++
@@ -1,9 +0,0 @@
-rmdir package
-dotnet pack error-reporting-csharp --configuration Release -o package > packageoutput
-::SET NUGETPACKAGEPATH
-
-::https://stackoverflow.com/questions/108439/how-do-i-get-the-result-of-a-command-in-a-variable-in-windows
-ExtractNugetPackagePath\ExtractNugetPackagePath.exe
-call setnpp.bat
-echo %NUGETPACKAGEPATH%
-
